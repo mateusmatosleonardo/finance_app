3fea33c332aa8009656e037bc6f4c254b5f95a1a
Mateus Leonardo <m4teusmatos@gmail.com>
2023-01-20 12:44:35 -0300

    refactor: refactoring file App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Routes } from './src/routes/Routes';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
+// import { Loading } from './src/components/Loading/Loading';
+// import FlashMessage from 'react-native-flash-message';
+import { theme } from './src/theme/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null
+    // return <Loading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <Routes />
+        <StatusBar style="auto" />
+        {/* <FlashMessage position="top" /> */}
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
