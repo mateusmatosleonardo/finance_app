b0ee6fc1ca72667fec2d1e10f0b187434b327dab
Mateus Leonardo <m4teusmatos@gmail.com>
2023-01-20 23:00:21 -0300

    creating file of theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  name: 'DefaultTheme',
+  colors: {
+    WHITE: '#ffffff',
+    BLACK: '#171b2f',
+    PRIMARY_GRADIENT: '#5561fc',
+    SECONDARY_GRADIENT: '#864cf8',
+    GRAY: '#EEEEEE',
+    OTHER_GRAY: '#f6f7ff',
+    // BLUE: '#3175e6',
+    // BLUE_SECONDARY: '#679ad8',
+    // BLUE_LIGHT: '#99bfe7',
+    // RED: '#ff375b',
+  },
+  fonts: {
+    Montserrat_Regular: 'Montserrat_400Regular',
+    Montserrat_Medium: 'Montserrat_500Medium',
+    Montserrat_SemiBold: 'Montserrat_600SemiBold',
+    Montserrat_Bold: 'Montserrat_700Bold',
+  },
+  sizes: {
+    SMALL: 12,
+    NORMAL: 14,
+    MEDIUM_LARGE: 16,
+    LARGE: 20,
+    ULTRA_LARGE: 22,
+  },
+};
