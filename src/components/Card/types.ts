06861c3924143a37d1631d83f12408dafb779c49
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-03 20:31:50 -0300

    feat: creating typing for the cards

--- src/components/Card/types.ts
+++ src/components/Card/types.ts
@@ -1,9 +1,10 @@
 import { ITransactions } from '../Transactions/types';
 
 export interface ICard {
-  id: number;
+  index: number;
   number_card: string;
   balance: string;
   transactions: ITransactions[];
   flag: 'cc-visa' | 'cc-mastercard' | 'cc-discover' | 'cc-amex' | 'cc-paypal' | 'cc-diners-club';
+  scrollX?: number;
 }
