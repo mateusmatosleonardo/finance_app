5e185626aa2e78c5bb0fec5e0cdc4a69c03c72b7
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-03 20:38:35 -0300

    feat: creating credit card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,19 +1,53 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
+import { Dimensions } from 'react-native';
+import { ICard } from './types';
 import * as S from './styles';
 import DotSingle from '@expo/vector-icons/Entypo';
 import CreditCard from '@expo/vector-icons/FontAwesome';
-import { ICard } from './types';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useSharedValue,
+  useAnimatedStyle
+} from 'react-native-reanimated';
 
-export function Card({ id, number_card, balance, flag }: ICard) {
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const SPACING = SLIDER_WIDTH * 0.02;
+const SIDECARD_LENGHT = (SLIDER_WIDTH * 0.12) / 2;
+const CARD_LENGTH = SLIDER_WIDTH * 0.88;
+
+export function Card({ index, number_card, balance, flag, scrollX }: ICard) {
+
+  const size = useSharedValue(0.8);
+
+  const inputRange = [
+    (index - 1) * CARD_LENGTH,
+    index * CARD_LENGTH,
+    (index + 1) * CARD_LENGTH
+  ];
+
+  size.value = interpolate(
+    //@ts-ignore
+    scrollX,
+    inputRange,
+    [0.8, 1, 0.8],
+    Extrapolate.CLAMP
+  );
+
+  const cardStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scaleY: size.value }],
+    }
+  });
+
   return (
-    <LinearGradient
-      key={id}
-      style={S.styles.wrapperCard}
-      colors={['#5165ff', '#7070ff']}
-      start={{ x: 0.5, y: 0 }}
-      end={{ x: 0.5, y: 1 }}
-      locations={[0.2, 0.6]}
+    <Animated.View
+      key={index}
+      style={[S.styles.wrapperCard, cardStyle, {
+        marginLeft: index === 0 ? SIDECARD_LENGHT : SPACING,
+        marginRight: index === 2 ? SIDECARD_LENGHT : SPACING,
+        backgroundColor: index === 0 ? '#7070ff' : index === 1 ? '#3175e6' : '#99bfe7'
+      }]}
     >
       <S.WrapperCardNumber>
         <DotSingle name='dot-single' size={16} color='#ffffff' />
@@ -29,6 +63,6 @@ export function Card({ id, number_card, balance, flag }: ICard) {
         </S.WrapperBalance>
         <CreditCard name={flag} size={40} color='#fafafa' />
       </S.Footer>
-    </LinearGradient>
+    </Animated.View >
   );
 }
