75911f227effe7024e60145ed49bd36e5b5d896c
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-03 21:46:58 -0300

    feat: creating object for reuse of repeated codes

---
+++ src/utils/measurements.ts
@@ -0,0 +1,11 @@
+import { Dimensions } from 'react-native';
+
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const SPACING = SLIDER_WIDTH * 0.02;
+const CARD_LENGTH = SLIDER_WIDTH * 0.88;
+
+export const MEASUREMENTS = {
+  SLIDER_WIDTH,
+  SPACING,
+  CARD_LENGTH,
+};
