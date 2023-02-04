dbb39ff7c91bbf31309f0c58b57ed2bec432d0d5
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-03 22:12:23 -0300

    refactor: refatoring button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
 import * as S from './styles';
+import { LinearGradient } from 'expo-linear-gradient';
 import { IButton } from './types';
 import Icon from '@expo/vector-icons/FontAwesome';
 
-export function Button({ icon,
-  style,
-  colors,
+export function Button({
   title,
+  icon,
+  size_icon,
   color_icon,
-  size_icon }: IButton) {
+  color_is_linear,
+  colors,
+  style }: IButton) {
   return (
-    <LinearGradient
-      style={style}
-      colors={colors}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
-    >
-      {title ? <S.Title>{title}</S.Title> : ''}
-      <Icon name={icon} size={size_icon} color={color_icon} />
-    </LinearGradient>
+    <>
+      {color_is_linear ? (<LinearGradient
+        style={style}
+        colors={colors}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        {title ? <S.Title>{title}</S.Title> : ''}
+        <Icon name={icon} size={size_icon} color={color_icon} />
+      </LinearGradient>) :
+        (<S.Button style={style}>
+          {title ? <S.Title>{title}</S.Title> : ''}
+          <Icon name={icon} size={size_icon} color={color_icon} />
+        </S.Button>)}
+    </>
   );
 }
