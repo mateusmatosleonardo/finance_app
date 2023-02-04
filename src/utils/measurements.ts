72095231e87379e4ce62ed456a04e532e9e0f480
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-03 21:54:02 -0300

    chore: adding new measurement

--- src/utils/measurements.ts
+++ src/utils/measurements.ts
@@ -3,9 +3,11 @@ import { Dimensions } from 'react-native';
 const SLIDER_WIDTH = Dimensions.get('window').width;
 const SPACING = SLIDER_WIDTH * 0.02;
 const CARD_LENGTH = SLIDER_WIDTH * 0.88;
+const SIDECARD_LENGHT = (SLIDER_WIDTH * 0.12) / 2;
 
 export const MEASUREMENTS = {
   SLIDER_WIDTH,
   SPACING,
   CARD_LENGTH,
+  SIDECARD_LENGHT,
 };
